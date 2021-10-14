3556d3d9ffadb604cd38a89d095f01dc1cf9632f
pavani addanki <pavani18.devops@gmail.com>
2021-10-13 22:01:00 -0400

    first commit

---
+++ twosum.py
@@ -0,0 +1,21 @@
+##Two sum 
+
+def twosum(i,s):
+    print("The sum need to be checked is {}".format(s))
+    print("The array entered is {}".format(i))
+    
+    
+lst[]
+n = int(input("Enter the number of elements"))
+print("Enter the elements")
+for i in range(0,n)
+    int(input(i))
+    lst.append(i)
+    
+print("Array entered by user is {}".format(lst))
+
+s = int(input("Enter the sum which needs to be checked"))
+
+
+
+twosum(lst,s)
